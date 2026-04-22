2a925ceacbbee74997f17391c800ec5c7c3519e8
Gerald C. Nelson <nelson.gerald.c@gmail.com>
2026-04-21 20:14:35 -0600

    Another modified md code to use .wepb files instead of jpg.

--- biochar_app/markdown/tools/markdown_config.py
+++ biochar_app/markdown/tools/markdown_config.py
@@ -192,7 +192,7 @@ docx_markdown_config: DocxMarkdownConfig = {
         "output_md": "experimentDesign.md",
         "images": [
             {
-                "file": "biocharExperimentalDesign.png",
+                "file": "biocharExperimentalDesign.webp",
                 "caption": "Figure 1: Field experimental layout",
                 "alt": "Layout of biochar plots",
                 "title": "Layout of biochar plots",
